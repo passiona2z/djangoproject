be6277c27c2d5d7e5e5f7cf49412e718824892ea
seonjin <yellow3978@gmail.com>
2021-07-13 10:49:22 +0900

    7/13 djangoProject commit end

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
 
 # 아주 간단한 뷰
+from django.urls import reverse
+from django.views.generic import CreateView
+
 from accountapp.models import NewModel
 
 
@@ -17,8 +22,16 @@ def hello_world(request):
         model_instance.text = temp
         model_instance.save()
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'model_instance': model_instance})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+
     else :
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context = {'text': 'POST METHOD!'})
+                      context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url =
